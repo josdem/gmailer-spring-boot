d8d7622d3b53304271d849d3d4299224e38c8cdd
josdem <joseluis.delacruz@gmail.com>
2025-05-09 10:59:29 -0600

    Adding application configuration

--- src/main/java/com/josdem/gmail/config/ApplicationConfig.java
+++ src/main/java/com/josdem/gmail/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.josdem.gmail.config;
 
 import com.sun.net.httpserver.HttpServer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,16 +9,19 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final ApplicationProperties applicationProperties;
 
     @Bean
     HttpServer httpServer() {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(applicationProperties.getPort()), 0);
             server.setExecutor(null);
             return server;
-        } catch (IOException e) {
-            throw new RuntimeException("Error starting HTTP server", e);
+        } catch (IOException ioe) {
+            throw new RuntimeException("Error starting HTTP server", ioe);
         }
     }
 }
