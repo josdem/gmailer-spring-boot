d3eee2086e6ecc405a5f3e8c0722a38d40cf9d9d
josdem <joseluis.delacruz@gmail.com>
2025-05-08 18:47:58 -0600

    Adding parameters

--- src/main/java/com/josdem/gmail/GmailSpringBootApplication.java
+++ src/main/java/com/josdem/gmail/GmailSpringBootApplication.java
@@ -9,7 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @SpringBootApplication
@@ -23,7 +26,7 @@ public class GmailSpringBootApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		try {
 			HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-			server.createContext("/hello", new HelloHandler());
+			server.createContext("/send", new SenderHandler());
 			server.setExecutor(null);
 			server.start();
 			System.out.println("Server started on port 8080");
@@ -33,15 +36,50 @@ public class GmailSpringBootApplication implements CommandLineRunner {
 		}
 	}
 
-	static class HelloHandler implements HttpHandler {
+	static class SenderHandler implements HttpHandler {
+
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
-			String response = "OK";
+			String response = "Use /get?hello=word";
 			exchange.sendResponseHeaders(200, response.getBytes().length);
 			try (var os = exchange.getResponseBody()) {
 				os.write(response.getBytes());
 			}
 		}
+	}
+
+	public static void writeResponse(HttpExchange httpExchange, String response) throws IOException {
+		httpExchange.sendResponseHeaders(200, response.length());
+		OutputStream os = httpExchange.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+	}
+
+
+	static class GetHandler implements HttpHandler {
+		public void handle(HttpExchange httpExchange) throws IOException {
+			StringBuilder response = new StringBuilder();
+			Map<String,String> parms = queryToMap(httpExchange.getRequestURI().getQuery());
+			response.append("<html><body>");
+			response.append("hello : " + parms.get("hello") + "<br/>");
+			response.append("foo : " + parms.get("foo") + "<br/>");
+			response.append("</body></html>");
+			writeResponse(httpExchange, response.toString());
+		}
+	}
+
+	public static Map<String, String> queryToMap(String query){
+		Map<String, String> result = new HashMap<>();
+		for (String param : query.split("&")) {
+			String pair[] = param.split("=");
+			if (pair.length>1) {
+				result.put(pair[0], pair[1]);
+			}
+			else{
+				result.put(pair[0], "");
+			}
+		}
+		return result;
 	}
 
 }
