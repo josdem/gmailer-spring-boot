7d643cf311a83a4153cf88a9ed7504d1d9cd9a29
josdem <joseluis.delacruz@gmail.com>
2025-05-10 11:10:17 -0600

    #3 Adding email creator class

---
+++ src/main/java/com/josdem/gmail/mail/EmailCreator.java
@@ -0,0 +1,47 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.josdem.gmail.mail;
+
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Component
+public class EmailCreator {
+
+    public MimeMessage create(String toEmailAddress,
+                                     String fromEmailAddress,
+                                     String subject,
+                                     String bodyText)
+            throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress(fromEmailAddress));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(toEmailAddress));
+        email.setSubject(subject);
+        email.setText(bodyText);
+        return email;
+    }
+}
