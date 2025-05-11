ddbb55de92eee9562eca123495f4a13b598666d2
josdem <joseluis.delacruz@gmail.com>
2025-05-11 11:42:00 -0600

    Setting build Gradle with Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+    id ("com.diffplug.spotless") version "7.0.2"
+    id("org.jetbrains.kotlin.jvm") version "2.1.10"
+    id ("java")
+}
+
+val googleApiClientVersion = "2.0.0"
+val googleOauthClientVersion = "1.34.1"
+val googleApiServicesVersion = "v1-rev20220404-2.0.0"
+val mockitoKotlinVersion = "5.4.0"
+val javaMailVersion = "1.6.2"
+
+group = "com.josdem.gmail"
+version = "1.0.0"
+
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        removeUnusedImports()
+        endWithNewline()
+    }
+    kotlin {
+        target("**/*.kt")
+        targetExclude("**/build/**", "**/build-*/**")
+        ktlint()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
+dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-web")
+    implementation ("com.google.api-client:google-api-client:$googleApiClientVersion")
+    implementation ("com.sun.mail:javax.mail:$javaMailVersion")
+    implementation ("com.google.oauth-client:google-oauth-client-jetty:$googleOauthClientVersion")
+    implementation ("com.google.apis:google-api-services-gmail:$googleApiServicesVersion")
+    compileOnly ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
+    testCompileOnly ("org.projectlombok:lombok")
+    testAnnotationProcessor ("org.projectlombok:lombok")
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    systemProperties(System.getProperties().toMap() as Map<String,Object>)
+}
+
+tasks.withType<Test> {
+    dependsOn("spotlessApply")
+}
