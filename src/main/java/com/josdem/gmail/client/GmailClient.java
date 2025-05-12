8932888dbce62abe7e62da77973c2908c8765aba
josdem <joseluis.delacruz@gmail.com>
2025-05-12 16:06:13 -0600

    Adding tokens path

--- src/main/java/com/josdem/gmail/client/GmailClient.java
+++ src/main/java/com/josdem/gmail/client/GmailClient.java
@@ -39,7 +39,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class GmailClient {
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  private static final String TOKENS_DIRECTORY_PATH = "src/main/resources/tokens";
   private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
   private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
