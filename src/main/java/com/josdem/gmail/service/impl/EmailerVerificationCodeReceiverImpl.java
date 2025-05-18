998fb72a30feab75ab0812aaaa9ae96168539ef1
Tim <luismuraya11@gmail.com>
2025-05-18 16:43:39 +0300

    Added pointcut, ready for testing

--- src/main/java/com/josdem/gmail/service/impl/EmailerVerificationCodeReceiverImpl.java
+++ src/main/java/com/josdem/gmail/service/impl/EmailerVerificationCodeReceiverImpl.java
@@ -18,27 +18,29 @@ package com.josdem.gmail.service.impl;
 
 import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
 import com.josdem.gmail.config.ApplicationProperties;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 
 @Component
 @RequiredArgsConstructor
 public class EmailerVerificationCodeReceiverImpl implements VerificationCodeReceiver {
 
-  private final ApplicationProperties applicationProperties;
+    private final ApplicationProperties applicationProperties;
 
-  @Override
-  public String getRedirectUri() throws IOException {
-    return applicationProperties.getCallbackUrl();
-  }
+    @Override
+    public String getRedirectUri() throws IOException {
+        return applicationProperties.getCallbackUrl();
+    }
 
-  @Override
-  public String waitForCode() throws IOException {
-      return "";
-  }
+    @Override
+    public String waitForCode() throws IOException {
+        return "";
+    }
 
-  @Override
-  public void stop() throws IOException {}
+    @Override
+    public void stop() throws IOException {
+    }
 }
