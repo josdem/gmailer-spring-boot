3f5f7165d2ef2d03a170b33208b0c618debb8aa8
josdem <joseluis.delacruz@gmail.com>
2025-07-10 16:25:27 -0600

    #9 Set specific location for credentials file

--- build.gradle.kts
+++ build.gradle.kts
@@ -122,8 +122,8 @@ tasks.jacocoTestReport {
 }
 
 tasks.register<Copy>("copyCredentials") {
-    //I'm assuming I don't have access of credentials.json
-    val source = file("credentials.json")
+    // This task copies the credentials.json file from a specific location to the resources directory
+    val source = file("/home/josdem/.gmailer/credentials.json")
     val destination = file("src/main/resources/")
 
     from(source)
