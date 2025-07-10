2d774f6e50450fc3d96ba8d235ae8c871ef913c9
josdem <joseluis.delacruz@gmail.com>
2025-07-10 16:52:24 -0600

    #9 Adding helpers for easy testing

--- src/main/java/com/josdem/gmail/mail/EmailCreator.java
+++ src/main/java/com/josdem/gmail/mail/EmailCreator.java
@@ -1,9 +1,26 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.josdem.gmail.mail;
 
+import com.josdem.gmail.helper.InternetAddressHelper;
+import com.josdem.gmail.helper.MimeMessageHelper;
+import com.josdem.gmail.helper.SessionHelper;
 import java.util.Properties;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,16 +29,22 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class EmailCreator {
 
+  private final Properties properties;
+  private final SessionHelper sessionHelper;
+  private final MimeMessageHelper mimeMessageHelper;
+  private final InternetAddressHelper internetAddressHelper;
+
   public MimeMessage create(
       String toEmailAddress, String fromEmailAddress, String subject, String bodyText)
       throws MessagingException {
-    Properties props = new Properties();
-    Session session = Session.getDefaultInstance(props, null);
 
-    MimeMessage email = new MimeMessage(session);
+    var session = sessionHelper.createSession(properties);
+    var email = mimeMessageHelper.createMimeMessage(session);
 
-    email.setFrom(new InternetAddress(fromEmailAddress));
-    email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(toEmailAddress));
+    email.setFrom(internetAddressHelper.createInternetAddress(fromEmailAddress));
+    email.addRecipient(
+        javax.mail.Message.RecipientType.TO,
+        internetAddressHelper.createInternetAddress(toEmailAddress));
     email.setSubject(subject);
     email.setText(bodyText);
     return email;
