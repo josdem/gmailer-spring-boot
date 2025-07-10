2d65d737688f90bf64d8076774311450f86306cf
josdem <joseluis.delacruz@gmail.com>
2025-07-10 17:17:29 -0600

    #9 Adding email creator test

--- src/test/java/com/josdem/gmail/mail/EmailCreatorTest.kt
+++ src/test/java/com/josdem/gmail/mail/EmailCreatorTest.kt
@@ -16,8 +16,77 @@
 
 package com.josdem.gmail.mail
 
+import com.josdem.gmail.helper.InternetAddressHelper
+import com.josdem.gmail.helper.MimeMessageHelper
+import com.josdem.gmail.helper.SessionHelper
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.verify
 import org.slf4j.LoggerFactory
+import java.util.Properties
+import javax.mail.Session
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 internal class EmailCreatorTest {
+    @Mock
+    private lateinit var properties: Properties
+
+    @Mock
+    private lateinit var sessionHelper: SessionHelper
+
+    @Mock
+    private lateinit var mimeMessageHelper: MimeMessageHelper
+
+    @Mock
+    private lateinit var internetAddressHelper: InternetAddressHelper
+
+    @Mock
+    private lateinit var session: Session
+
+    @Mock
+    private lateinit var mimeMessage: MimeMessage
+
+    @Mock
+    private lateinit var fromInternetAddress: InternetAddress
+
+    @Mock
+    private lateinit var toInternetAddress: InternetAddress
+
+    @InjectMocks
+    private lateinit var emailCreator: EmailCreator
+
+    @BeforeEach
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+    }
+
     private val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `should create mime message`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+
+        val fromEmailAddress = "vetlog@josdem.io"
+        val toEmailAddress = "contact@josdem.io"
+        val subject = "Test Subject"
+        val body = "Test Body"
+
+        `when`(sessionHelper.createSession(properties)).thenReturn(session)
+        `when`(mimeMessageHelper.createMimeMessage(session)).thenReturn(mimeMessage)
+        `when`(internetAddressHelper.createInternetAddress(fromEmailAddress)).thenReturn(fromInternetAddress)
+        `when`(internetAddressHelper.createInternetAddress(toEmailAddress)).thenReturn(toInternetAddress)
+
+        emailCreator.create(toEmailAddress, fromEmailAddress, subject, body)
+
+        verify(mimeMessage).setFrom(fromInternetAddress)
+        verify(mimeMessage).addRecipient(MimeMessage.RecipientType.TO, toInternetAddress)
+        verify(mimeMessage).subject = subject
+        verify(mimeMessage).setText(body)
+    }
 }
